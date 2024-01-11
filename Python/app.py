aba4abe805dcac4f09f0a58372aa61e3e00a38bb
Tyler Teichmann <100450467+tylerteichmann@users.noreply.github.com>
2024-01-11 21:49:59 +0000

    python exercises

---
+++ Python/app.py
@@ -0,0 +1,33 @@
+# exercise 1: remove the space before each period
+# wage = 20
+# hours = 40
+# w = "Your wage rate is"
+# h = "Your hours worked are"
+# earnings = wage * hours
+# e = "Your earnings are"
+# p = "."
+
+# print(f"{w} {wage}{p}")
+# print(h, hours, end="")
+# print(p)
+# print(e, earnings, end="")
+# print(p)
+
+# exercies 2: print the letters A, B, and C on a new line with one statement
+# print("A\nB\nC")
+
+# exercise 3: Ask for the name of a person's pet, wait for an answer, 
+# then print the name of the pet with a comment
+# pet_name = input("What is your pet's name? ")
+
+# print(f"{pet_name} is an awesome name!")
+
+# exercise 4: Request an integer, multiply by 2, print the result.
+
+# x = int(input("Input an integer: "))
+
+# y = x * 2
+
+# print(y)
+
+# exercise 5:
