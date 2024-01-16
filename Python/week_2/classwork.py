9683d8d9f7624196d2af552c17a8c47cd3017023
Tyler Teichmann <100450467+tylerteichmann@users.noreply.github.com>
2024-01-16 22:04:27 +0000

    classwork

--- Python/week_2/classwork.py
+++ Python/week_2/classwork.py
@@ -1,6 +1,35 @@
+import math
+
 def main():
+# the - has lower precidence than **
+#     print(- 1 ** 2)
+#     print(- 1 * - 1)
+
+# compoud operators
+    # x = 1
+    # x *= 100
+    # x += 10
+    # x -= 20
+    # x /= 2
+    # x **=.5
+
+    # print(x)
+
+    # x = 5 / 3
+    # print(x)
+
+    # x = 5 // 3
+    # print(x)
+
+    # x = 5 % 3
+    # print(x)
+
+    print("TomDickHarry")
+    print("Tom\nDick\nHarry")
+    print("Tom\\nDick\\nHarry")
+    print("Tom\\\Dick\\\Harry")
+
     return 1
 
-
 if __name__ == "__main__":
-    main
+    main()
