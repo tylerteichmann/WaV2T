f171187312a6b72b7d4b467e5bbc35be63ee251a
Tyler Teichmann <100450467+tylerteichmann@users.noreply.github.com>
2024-01-19 00:34:43 +0000

    end of day

--- CSC160/week_2/classwork.py
+++ CSC160/week_2/classwork.py
@@ -24,10 +24,50 @@ def main():
     # x = 5 % 3
     # print(x)
 
-    print("TomDickHarry")
-    print("Tom\nDick\nHarry")
-    print("Tom\\nDick\\nHarry")
-    print("Tom\\\Dick\\\Harry")
+    # print("TomDickHarry")
+    # print("Tom\nDick\nHarry")
+    # print("Tom\\nDick\\nHarry")
+    # print("Tom\\\Dick\\\Harry")
+    # print(f"each\and")
+
+#     num_books = 2
+#     price = 30
+#     s = f'I bought {num_books} books for {price} each\
+#  and paid ${num_books * price:.2f}.'
+#     print(s)
+    
+    # names = [ 'Tom', 'Dick', 'Harry' ]
+    # # print(names)
+
+    # names2 = ['Susan', 'Alice', 'Beth']
+    # namesb = names + names2
+    # print(namesb)
+
+    # t1 = 1, 2, 3
+
+    # x = t1[0]
+    # print(x)
+
+    # x = t1[-1]
+    # print(x)
+
+    # ASet = { "cat", "dog", "parrot" }
+    # AList = [ "cat", "dog", "parrot" ]
+    # ATuple = "cat", "dog", "parrot"
+
+    # print(type(ASet[0]))
+    # print(type(AList[0]))
+    # print(type(ATuple[0]))
+
+    adict = {
+        "Joe": 175,
+        "Tom": 190,
+        "Dick": 150
+    }
+
+    adict["Harry"] = 180
+
+    print(adict["Dick"])
 
     return 1
 
