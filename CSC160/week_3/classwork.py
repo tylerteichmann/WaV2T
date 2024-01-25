93f95ae1314f682a9fd50dc63e0a8b4b8ee9f705
Tyler Teichmann <100450467+tylerteichmann@users.noreply.github.com>
2024-01-25 00:07:16 +0000

    end of day commit

--- CSC160/week_3/classwork.py
+++ CSC160/week_3/classwork.py
@@ -84,39 +84,39 @@ def main():
     if input_month == 'January' and 0 < input_day <= 31:
         print("Winter")
     elif input_month == 'February' and 0 < input_day <= 28:
-        print("Winter")   
+        print("Winter")
     elif input_month == 'March' and 0 < input_day <= 31:
         if input_day < 20:
             print("Winter")
         else:
             print("Spring")
     elif input_month == 'April' and 0 < input_day <= 30:
-        print("Spring")   
+        print("Spring")
     elif input_month == 'May' and 0 < input_day <= 31:
-        print("Spring")   
+        print("Spring")
     elif input_month == 'June' and 0 < input_day <= 30:
         if input_day < 21:
             print("Spring")
         else:
             print("Summer")
     elif input_month == 'July' and 0 < input_day <= 31:
-        print("Summer")   
+        print("Summer")
     elif input_month == 'August' and 0 < input_day <= 31:
-        print("Summer")   
+        print("Summer")
     elif input_month == 'September' and 0 < input_day <= 30:
         if input_day < 22:
             print("Summer")
         else:
-            print("Autumn")    
+            print("Autumn")
     elif input_month == 'October' and 0 < input_day <= 31:
-        print("Autumn")   
+        print("Autumn")
     elif input_month == 'November' and 0 < input_day <= 30:
-        print("Autumn")   
+        print("Autumn")
     elif input_month == 'December' and 0 < input_day <= 31:
         if input_day < 22:
             print("Autumn")
         else:
-            print("Winter")    
+            print("Winter")
     else:
         print("Invalid")
 
