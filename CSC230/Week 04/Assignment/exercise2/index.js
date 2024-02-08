237d8fd897ed493389c79e0d9574a8e9ceb83558
tteichmann <tyler.teichmann@stmartin.edu>
2024-02-08 13:04:35 -0800

    update

--- CSC230/Week 04/Assignment/exercise2/index.js
+++ CSC230/Week 04/Assignment/exercise2/index.js
@@ -1,3 +1,45 @@
+// This is my first JavaScript code!
+console.log("Hello World");
+
+let name = "Mosh";
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = "Mosh";
+let lastName = "Hamedani";
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let name = "Mosh"; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
+
+let person = {
+    name: "Mosh",
+    age: 30
+};
+
+// Dot Notaiton
+person.name = "John";
+
+// Bracket Notation
+let selection = "name";
+person[selection] = "Mary";
+
+console.log(person.name);
+
+let selectedColors = ["red", "blue"];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
 
 //Performing a task
 function greet(name, lastName){
