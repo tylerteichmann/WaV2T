66deeece66344f789dd33e4fc95815c01a23b51d
tteichmann <tyler.teichmann@stmartin.edu>
2024-02-16 10:08:50 -0800

    end of class commmit

--- CSC160/week_6/labs/sorting.py
+++ CSC160/week_6/labs/sorting.py
@@ -1,12 +1,18 @@
-lines = []
-
-with open(input(), 'r') as file:
-    lines.append(file.readlines())
-
 shows = {}
 
-for i in range(0, len(lines), 2):
-    
-    
+with open(input(), 'r') as file:
+    for line in file:
+        if int(line.rstrip()) in list(shows.keys()):
+            shows[int(line.rstrip())].append(file.readline().rstrip())
+        else:
+            shows[int(line.rstrip())] = [ file.readline().rstrip() ]
 
-print(shows)
+
+with open("output_keys.txt", 'w') as file:
+    for item in sorted(shows.items(), reverse=True):
+        file.write(f"{str(item[0])}: {'; '.join(item[1])}\n")
+
+
+with open("output_titles.txt", 'w') as file:
+    string = f'{"\\n".join(sorted([show for value in shows.values() for show in value], reverse=True))}'
+    file.write(string)
