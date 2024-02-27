21764be0eb359572a1459dae2e18ca34a3cbfe2e
tteichmann <tyler.teichmann@stmartin.edu>
2024-02-27 15:34:48 -0800

    end of class commmit

---
+++ CSC160/week_8/main.py
@@ -0,0 +1,23 @@
+# def list_sum(num_list):
+#     if len(num_list) == 0:
+#         return 0
+#     return num_list[0] + list_sum(num_list[1:])
+
+# print(list_sum([1,3,5,7,9]))
+
+# def flatten(in_list):
+#     new_list = []
+#     for i in in_list:
+#         if isinstance(i, list):
+#             new_list.extend(flatten(i))
+#         else:
+#             new_list.append(i)
+#     return new_list
+
+# print(flatten([1,[2,3,[4,5,6]]]))
+
+# def fib(n):
+
+
+
+# print(fib(3))
