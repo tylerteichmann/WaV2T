7be9bb2e0b26a1eeda8fdcb582c8bdeb2d9a2b84
tteichmann <tyler.teichmann@stmartin.edu>
2024-04-08 11:38:24 -0700

    Started Homework 5 for algorithms

---
+++ csc295/week_05/Homework 5 - Algorithms/Node.java
@@ -0,0 +1,27 @@
+//Tyler J. Teichmann
+
+// Node class for a doubly linked list.
+public class Node<T> {
+
+    // Data to store of type T.
+    T data;
+    // Pointer for the next node in the list.
+    Node<T> next;
+    // Pointer for the previous node in the list.
+    Node<T> previous;
+
+    // Constructor for the node if the place in the list is not specified.
+    public Node(T data) {
+        // Set the data in the node.
+        this.data = data;
+        // Set the next node in the list.
+        this.next = null;
+        // Set the previous node in the list.
+        this.previous = null;
+    }
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+}
